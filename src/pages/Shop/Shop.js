52e97eeb032401c9bb5842c68a65a85b2d8d0c9a
Alukesh <akenzhebaev422@gmail.com>
2022-04-26 17:01:27 +0600

    26 april paginations

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,10 +1,14 @@
 import About from "../Brands/About/About";
 import {useState, useContext} from "react";
+import 'antd/dist/antd.css';
+import { Pagination } from 'antd';
+// import {createRoot} from "react-dom/client";
+// import Demo from './demo'
 import {CustomContext} from "../../Context";
-import CollectionCard from "../Home/Collection/CollectionCard";
 
 const Shop = () => {
     const [status, setStatus] = useState('');
+    const [page, setPage] = useState(1);
     const {shop} = useContext(CustomContext);
 
 
@@ -12,21 +16,26 @@ const Shop = () => {
         <section className={'shop'}>
             <About title={"Shop.title"} link1={"Shop.link1"} link2={"Shop.link2"}/>
             <ul className={'shop__list'}>
-                <li className={`shop__item ${status === '' && 'shop__item_active'}`} onClick={() => setStatus('')}>Все</li>
-                <li className={`shop__item ${status === 'coat' && 'shop__item_active'}`} onClick={() => setStatus('coat')}>Пальто</li>
-                <li className={`shop__item ${status === 'sweatshirt' && 'shop__item_active'}`} onClick={() => setStatus('sweatshirt')}>Свитшоты</li>
-                <li className={`shop__item ${status === 'cardigan' && 'shop__item_active'}`} onClick={() => setStatus('cardigan')}>Кардиганы</li>
-                <li className={`shop__item ${status === 'hoodie' && 'shop__item_active'}`} onClick={() => setStatus('hoodie')}>Толстовки</li>
+                <li className={`shop__item ${status === '' && 'shop__item_active'}`} onClick={() => {setPage(1); setStatus('')}}>Все</li>
+                <li className={`shop__item ${status === 'coat' && 'shop__item_active'}`} onClick={() => {setPage(1); setStatus('coat')}}>Пальто</li>
+                <li className={`shop__item ${status === 'sweatshirt' && 'shop__item_active'}`} onClick={() => {setPage(1); setStatus('sweatshirt')}}>Свитшоты</li>
+                <li className={`shop__item ${status === 'cardigan' && 'shop__item_active'}`} onClick={() => {setPage(1); setStatus('cardigan')}}>Кардиганы</li>
+                <li className={`shop__item ${status === 'hoodie' && 'shop__item_active'}`} onClick={() => {setPage(1); setStatus('hoodie')}}>Толстовки</li>
             </ul>
             Shops
-            <p>Показано: 9 из {shop.length} товаров</p>
+            <p>Показано: {shop.filter(item => item.category.includes(status)).filter((item, idx) => {
+                return  idx + 1 <= page * 9 && idx + 1 > page * 9 - 9
+            }).length} из
+                {shop.filter((item) => item.category.includes(status)).length} товаров</p>
 
             <div className={'shop__row'}>
                 {
                     shop.filter(item =>{
                         return item.category.includes(status)
+                    }).filter((item, idx) =>{
+                        return idx + 1 <= page * 9 && idx + 1 > page * 9 - 9
                     }).map(item =>(
-                            <div className="shop__card">
+                            <div key={item.id} className="shop__card">
                                 <div className="shop__hover">
                                     <img className={'shop__img'} src={`../Shop/${item.image}`} />
                                     <span className={`shop__arrow `}>
@@ -40,7 +49,21 @@ const Shop = () => {
                     ))
                 }
             </div>
-            <p>Показано: 9 из {shop.length} товаров</p>
+            <p>Показано: {
+                shop.filter(item => item.category.includes(status)
+                ).filter((item, idx) =>  idx + 1 <= page * 9 && idx + 1 > page * 9 - 9).length
+            } из {
+                shop.filter(item => item.category.includes(status)).length
+            } товаров</p>
+
+
+            {
+                shop.filter(item => item.category.includes(status)
+                ).length > 9 &&
+                <Pagination simple onChange={setPage} current={page} total={shop.filter(item =>
+                     item.category.includes(status)
+                ).length} pageSize={12}/>
+            }
 
         </section>
     );
